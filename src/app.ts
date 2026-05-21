3037ae20524266eef4ecafb61f94ad0eae1270f8
mdabdurrahman07 <jamilrahman2001@gmail.com>
2026-05-22 01:56:08 +0600

    implemented tsup and vercel config

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5000/",
-  }),
+    origin: ["https://vercel.app", "http://localhost:5000"],
+    credentials: true
+  })
 );
 
 // root
